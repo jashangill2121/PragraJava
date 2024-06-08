734618835a7739c67d374942e1ef5984f09f133a
JashanGill <satnamwaheguru21@gmail.com>
2024-06-08 01:53:43 -0400

    Assignments are done

--- src/Assignments/BankAccount.java
+++ src/Assignments/BankAccount.java
@@ -1,2 +1,82 @@
-package Assignments;public class BankAccount {
+package Assignments;
+
+public class BankAccount {
+
+                String accountNumber ;
+                String accountHolderName ;
+                double balance ;
+                static int numberOfAccounts = 0;
+
+
+       public BankAccount(){
+
+       }
+
+       public BankAccount (String accountNumber, String accountHolderName, double balance){
+
+                this.accountNumber = accountNumber;
+                this.accountHolderName = accountHolderName;
+                this.balance = balance;
+                numberOfAccounts++;
+
+          }
+
+       public static int countAccount(){
+
+                  return numberOfAccounts;
+
+       }
+
+       public void deposit( double amount){
+
+           balance += amount;
+
+       }
+
+       public void withdraw( double amount) {
+
+           if (balance>=amount) {
+
+               balance -= amount;
+           }
+
+           else {
+                   System.out.println("Insufficient funds");
+
+               }
+
+       }
+
+       public void checkBalance(double balance){
+
+           if (balance <= 0){
+
+               System.out.println("Your balance is 0 ");
+
+           }
+
+           else {
+
+               System.out.println("Your balance is " + balance);
+
+           }
+
+       }
+
+       public static void main(String[] args) {
+
+           BankAccount bankAccount = new BankAccount();
+           BankAccount bankAccount1 = new BankAccount("45215", "Aman", 500.0);
+           BankAccount bankAccount2 = new BankAccount("41215","Jashan",1000);
+           BankAccount bankAccount3 = new BankAccount("46587","Raman",569.44);
+
+           System.out.println( "Total accounts created are " + BankAccount.countAccount());
+
+           bankAccount1.deposit(500);
+           bankAccount1.withdraw(200);
+           bankAccount3.checkBalance(bankAccount1.balance);
+
+
+
+    }
 }
--- src/Assignments/Book.java
+++ src/Assignments/Book.java
@@ -1,2 +1,59 @@
-package Assignments;public class Book {
+package Assignments;
+
+public class Book {
+
+            String title ;
+            String author;
+            double price ;
+            int quantity ;
+
+
+    public Book() {
+
+    }
+
+
+    public Book (String title, String author, double price, int quantity) {
+
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            this.quantity = quantity;
+
+     }
+
+
+
+    public static void totalPrice(){
+
+             Book checkPrice = new Book("AsYouLikeIt","Shakespeare",50,10);
+
+             double totPrice = checkPrice.quantity * checkPrice.price;
+             System.out.println ("Total price of " + checkPrice.quantity + " books is $" + totPrice);
+
+         }
+
+
+    public void printDetails(){
+
+             System.out.println("Total price of " + quantity + " " + title + " books written by " + author + " is $" + quantity*price);
+
+         }
+
+
+    public static void main(String[] args) {
+
+             Book.totalPrice();
+
+             Book book1 = new Book();
+             Book book2 = new Book("Fable","Young",100.0,5);
+             Book book3 = new Book("Mockinjay","Collins",45,9);
+             Book book4 = new Book("WinterSong","Jones",30,4);
+
+             book1.printDetails();
+             book2.printDetails();
+             book3.printDetails();book4.printDetails();
+
+    }
+
 }
